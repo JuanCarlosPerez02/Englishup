0d26505311a513cdd75611a9d86ce5e5b117264a
JuanCarlosPerez02 <107712231+JuanCarlosPerez02@users.noreply.github.com>
2026-03-11 14:15:35 +0100

    Update vite.config.js

--- englishup/vite.config.js
+++ englishup/vite.config.js
@@ -3,4 +3,30 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022'
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
